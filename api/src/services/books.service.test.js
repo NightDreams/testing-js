d3f82b676171cb2efc6c7e9ab3d57c3fac257043
Angel Jair Neri Huerta <yayodcv@gmail.com>
2025-06-21 21:56:29 -0600

    test(boos.service): spy service with manual mock and different fakes per case

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -5,36 +5,46 @@ const fakeBooks = [
   { id: '2', name: 'To Kill a Mockingbird', author: 'Harper Lee' },
   { id: '3', name: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
 ];
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => { },
-};
 
-jest.mock('../lib/mongo.lib.js', () => jest.fn().mockImplementation(() => MongoLibStub));
+const mockGetAll = jest.fn();
+// .mockReturnValue(fakeBooks);
+
+jest.mock('../lib/mongo.lib.js', () => jest.fn().mockImplementation(
+  () => ({ getAll: mockGetAll, create: () => {} }),
+));
 
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
-    jest.clearAllMocks();
+    // jest.clearAllMocks();
   });
 
   describe('test for getBooks', () => {
     test('should return an list of books', async () => {
       // arrange
-      const books = await service.getBooks({});
+      mockGetAll.mockResolvedValue(fakeBooks);
       // act
-      console.log(books);
+      const books = await service.getBooks({});
+      // console.log(books);
       // assert
       expect(books.length).toEqual(3);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
     test('should return an list of books', async () => {
       // arrange
-      const books = await service.getBooks({});
+      mockGetAll.mockResolvedValue([{
+        _id: '1',
+        name: 'Hunger Games 2',
+        author: 'George Orwell',
+      }]);
       // act
+      const books = await service.getBooks({});
       console.log(books);
       // assert
-      expect(books[0].name).toEqual('Hunger Games');
+      expect(books[0].name).toEqual('Hunger Games 2');
     });
   });
 });
