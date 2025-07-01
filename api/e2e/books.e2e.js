d3fda9e7cb8bce6ed728e2620aead5302d1d2220
Angel Jair Neri Huerta <yayodcv@gmail.com>
2025-07-01 16:26:16 -0600

    test(bookService): mock integration test for GET /books

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const { generateManyBooks } = require('../src/fakes/book.fake');
+
+const mockGetAll = jest.fn();
+
+jest.mock('../src/lib/mongo.lib', () =>
+  jest.fn().mockImplementation(() => ({
+    getAll: mockGetAll,
+    create: () => {},
+  })),
+);
+
+const createApp = require('../src/app');
+
+describe('BookService: Test for books ', () => {
+  let app = null;
+  let server = null;
+
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('test(bookService): [integration|mock] GET /api/v1/books', () => {
+    test('should return a list of books', () => {
+      //arrange
+      const fakeBooks = generateManyBooks(3);
+      mockGetAll.mockResolvedValue(fakeBooks);
+
+      //act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          // console.log("🚀 integration 'books' body:", body);
+
+          //assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
